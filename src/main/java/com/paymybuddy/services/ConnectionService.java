45e305b11005ce181553356e1d6d7563cf84b116
Guillaume de Puget <guillaumedepuget@gmail.com>
2025-10-31 20:33:53 +0100

    chore: ajout de connectionService

---
+++ src/main/java/com/paymybuddy/services/ConnectionService.java
@@ -0,0 +1,8 @@
+package com.paymybuddy.services;
+
+
+public interface ConnectionService {
+
+    void addFriend(String userEmail, String friendEmail);
+    
+} 
---
+++ src/main/java/com/paymybuddy/services/ConnectionServiceImpl.java
@@ -0,0 +1,42 @@
+package com.paymybuddy.services;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.paymybuddy.exceptions.InvalidConnectionException;
+import com.paymybuddy.exceptions.UserNotFoundException;
+import com.paymybuddy.models.User;
+import com.paymybuddy.repositories.UserRepository;
+
+@Service
+public class ConnectionServiceImpl implements ConnectionService {
+
+    private final UserRepository userRepository;
+
+    public ConnectionServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    @Transactional
+    public void addFriend(String userEmail, String friendEmail) {
+
+        User user = userRepository.findByEmail(userEmail)
+        .orElseThrow(() -> new UserNotFoundException("Cet utilisateur n'a pas de compte Pay My Buddy.", userEmail));
+
+        User friend = userRepository.findByEmail(friendEmail)
+        .orElseThrow(() -> new UserNotFoundException("Cet utilisateur n'a pas de compte Pay My Buddy.", friendEmail));
+
+        if (friendEmail.equals(userEmail)) {
+            throw new InvalidConnectionException("Impossible de s'ajouter soi-même comme relation.", friendEmail);
+        }
+
+        if (user.getConnectionsWithFriends().contains(friend)) {
+            throw new InvalidConnectionException("Cette relation a déjà été enregistrée.", friendEmail);
+        }
+
+        user.getConnectionsWithFriends().add(friend);
+        friend.getConnectionsWithFriends().add(user);
+    }
+    
+} 
