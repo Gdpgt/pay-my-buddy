cc23a26011a217d7f7d43de43b81762495a0afda
Guillaume de Puget <guillaumedepuget@gmail.com>
2025-11-07 20:08:29 +0100

    chore: Ajout de TransferController

--- src/main/java/com/paymybuddy/services/UserService.java
+++ src/main/java/com/paymybuddy/services/UserService.java
@@ -1,6 +1,10 @@
 package com.paymybuddy.services;
 
+import com.paymybuddy.models.User;
+
 public interface UserService {
 
     void registerUser(String userEmail, String username, String userPassword);
+
+    User getUserByEmail(String email);
 }
