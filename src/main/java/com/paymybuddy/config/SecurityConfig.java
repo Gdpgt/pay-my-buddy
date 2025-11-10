b67bc64ea4edddbacd1fdf9e492dcec126c6d21a
Guillaume de Puget <guillaumedepuget@gmail.com>
2025-11-10 17:19:41 +0100

    test: Ajout de UserServiceTest

--- src/main/java/com/paymybuddy/config/SecurityConfig.java
+++ src/main/java/com/paymybuddy/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -34,8 +35,7 @@ public class SecurityConfig {
                 .failureUrl("/auth/login?error=true")
                 .permitAll()
             )
-            .logout(logout -> logout
-            .permitAll()
+            .logout(LogoutConfigurer::permitAll
             )
             .userDetailsService(userDetailsService);
 
