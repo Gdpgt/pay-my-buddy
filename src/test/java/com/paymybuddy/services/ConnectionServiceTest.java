c364296ff92c33fb2c32e4561c542b133b9c0e1e
Guillaume de Puget <guillaumedepuget@gmail.com>
2025-11-11 14:30:43 +0100

    test: ajout de ConnectionServiceTest

---
+++ src/test/java/com/paymybuddy/services/ConnectionServiceTest.java
@@ -0,0 +1,110 @@
+package com.paymybuddy.services;
+
+import static org.junit.jupiter.api.Assertions.assertThrowsExactly;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.paymybuddy.exceptions.InvalidConnectionException;
+import com.paymybuddy.exceptions.UserNotFoundException;
+import com.paymybuddy.models.User;
+import com.paymybuddy.repositories.UserRepository;
+
+@ExtendWith(MockitoExtension.class)
+public class ConnectionServiceTest {
+
+    @Mock
+    private UserRepository userRepository;
+
+    @InjectMocks
+    ConnectionServiceImpl connectionService;
+
+
+    @Test
+    void addFriend_shouldAddFriend_whenDataValid() {
+        
+        // Prepare
+        User userMock = new User("john@lennon.com", "john", "hashedPassword");
+        User friendMock = new User("yoko@ono.com", "yoko", "hashedPassword");
+        when(userRepository.findByEmail("john@lennon.com")).thenReturn(Optional.of(userMock));
+        when(userRepository.findByEmail("yoko@ono.com")).thenReturn(Optional.of(friendMock));
+
+        // Act
+        connectionService.addFriend("john@lennon.com", "yoko@ono.com");
+
+        // Assert
+        assertTrue(userMock.getConnectionsWithFriends().contains(friendMock));
+        assertTrue(friendMock.getConnectionsWithFriends().contains(userMock));
+    }
+
+
+    @Test
+    void addFriend_shouldThrowUserNotFoundException_whenUserNotExists() {
+
+        // Prepare
+        when(userRepository.findByEmail("john@lennon.com")).thenReturn(Optional.empty());
+
+        // Act & assert
+        assertThrowsExactly(UserNotFoundException.class,
+                            () -> connectionService.addFriend("john@lennon.com", "yoko@ono.com")
+        );
+    }
+
+
+    @Test
+    void addFriend_shouldThrowUserNotFoundException_whenFriendNotExists() {
+
+        // Prepare
+        User userMock = new User("john@lennon.com", "john", "hashedPassword");
+        when(userRepository.findByEmail("john@lennon.com")).thenReturn(Optional.of(userMock));  
+        when(userRepository.findByEmail("yoko@ono.com")).thenReturn(Optional.empty());
+
+        // Act & assert
+        assertThrowsExactly(UserNotFoundException.class,
+                            () -> connectionService.addFriend("john@lennon.com", "yoko@ono.com")
+        );
+    }
+
+
+    @Test
+    void addFriend_shouldThrowInvalidConnectionException_whenFriendIsUser() {
+
+        // Prepare
+        User userMock = new User("john@lennon.com", "john", "hashedPassword");
+        when(userRepository.findByEmail("john@lennon.com")).thenReturn(Optional.of(userMock));  
+
+        // Act & assert
+        assertThrowsExactly(InvalidConnectionException.class,
+                            () -> connectionService.addFriend("john@lennon.com", "john@lennon.com")
+        );
+    }
+
+
+    @Test
+    void addFriend_shouldThrowInvalidConnectionException_whenAlreadyAFriend() {
+
+        // Prepare
+        User userMock = new User("john@lennon.com", "john", "hashedPassword");
+        User friendMock = new User("yoko@ono.com", "yoko", "hashedPassword");
+        userMock.getConnectionsWithFriends().add(friendMock);
+        when(userRepository.findByEmail("john@lennon.com")).thenReturn(Optional.of(userMock));
+        when(userRepository.findByEmail("yoko@ono.com")).thenReturn(Optional.of(friendMock));
+
+        // Act & assert
+        assertThrowsExactly(InvalidConnectionException.class,
+                            () -> connectionService.addFriend("john@lennon.com", "yoko@ono.com")
+        );
+    }
+
+    
+}
