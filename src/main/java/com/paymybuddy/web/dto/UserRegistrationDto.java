8909540645aa43d0f7575928603eca452eff8781
Guillaume de Puget <guillaumedepuget@gmail.com>
2025-11-18 16:50:50 +0100

    style: ajout de common.css et amélio user-registration.css

--- src/main/java/com/paymybuddy/web/dto/UserRegistrationDto.java
+++ src/main/java/com/paymybuddy/web/dto/UserRegistrationDto.java
@@ -10,17 +10,17 @@ import lombok.Setter;
 @Setter
 public class UserRegistrationDto {
 
-    @NotBlank
-    @Size(min = 3, max = 50)
+    @NotBlank(message = "Le username ne peut pas être vide")
+    @Size(min = 3, max = 50, message = "La taille du username doit être comprise entre 3 et 50 caractères.")
     private String username;
 
-    @NotBlank
-    @Email
-    @Size(max = 255)
+    @NotBlank(message = "L'email ne peut pas être vide")
+    @Email(message = "Format d'email invalide")
+    @Size(max = 255, message = "La taille de l'email ne doit pas dépasser 255 caractères.")
     private String email;
 
-    @NotBlank
-    @Size(min = 8, max = 100)
+    @NotBlank(message = "Le mot de passe ne peut pas être vide")
+    @Size(min = 8, max = 100, message = "La taille du mot de passe doit être comprise entre 8 et 100 caractères.")
     private String password;
 
 }
