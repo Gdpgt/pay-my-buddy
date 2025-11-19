2212daa1fd787ec5ab487b746aebdbf76960805e
Guillaume de Puget <guillaumedepuget@gmail.com>
2025-11-19 10:26:26 +0100

    fix: correction encodage messages validation form

--- src/main/java/com/paymybuddy/web/dto/UserRegistrationDto.java
+++ src/main/java/com/paymybuddy/web/dto/UserRegistrationDto.java
@@ -10,17 +10,17 @@ import lombok.Setter;
 @Setter
 public class UserRegistrationDto {
 
-    @NotBlank(message = "Le username ne peut pas être vide")
-    @Size(min = 3, max = 50, message = "La taille du username doit être comprise entre 3 et 50 caractères.")
+    @NotBlank(message = "{validation.username.notblank}")
+    @Size(min = 3, max = 50, message = "{validation.username.size}")
     private String username;
 
-    @NotBlank(message = "L'email ne peut pas être vide")
-    @Email(message = "Format d'email invalide")
-    @Size(max = 255, message = "La taille de l'email ne doit pas dépasser 255 caractères.")
+    @NotBlank(message = "{validation.email.notblank}")
+    @Email(message = "{validation.email.invalid}")
+    @Size(max = 255, message = "{validation.email.size}")
     private String email;
 
-    @NotBlank(message = "Le mot de passe ne peut pas être vide")
-    @Size(min = 8, max = 100, message = "La taille du mot de passe doit être comprise entre 8 et 100 caractères.")
+    @NotBlank(message = "{validation.password.notblank}")
+    @Size(min = 8, max = 100, message = "{validation.password.size}")
     private String password;
 
 }
