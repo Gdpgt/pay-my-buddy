7207721bae0493981f27cac65f3893053c8ad3fb
DE PUGET Guillaume <guillaume.de.puget@xpfibre.com>
2025-11-25 17:40:51 +0100

    chore: amélio gitignore, config et readme

--- src/main/resources/db/V2__create_user_paymybuddy.sql
+++ src/main/resources/db/V2__create_user_paymybuddy.sql
@@ -1,9 +1,14 @@
--- Attention ! Remplacer les variables ${} par les vraies valeurs 
+-- Ce script a pour but de créer un user et mot de passe que l'app paymybuddy 
+-- pourra utiliser pour se connecter à la DB. C'est une bonne pratique qui évite 
+-- de laisser l'app tourner avec les identifiants 'root' superutilisateur
+-- de MySQL (qui autorise par exemple le table drop)
+
+-- ATTENTION !!! Remplacer les variables ${} par les vraies valeurs choisies
 
 CREATE USER IF NOT EXISTS '${DB_USER}'@'localhost'
   IDENTIFIED BY '${DB_PASSWORD}';
 
-GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, ALTER, INDEX, DROP
+GRANT SELECT, INSERT, UPDATE, DELETE
 ON paymybuddy.* TO '${DB_USER}'@'localhost';
 
 FLUSH PRIVILEGES;
